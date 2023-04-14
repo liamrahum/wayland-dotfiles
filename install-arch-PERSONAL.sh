2765fd57825dc25057332e6c70529ae71860ffda
Liam Rahum <liam.rahum@gmail.com>
2023-04-14 18:18:34 +0300

    .

--- install-arch-PERSONAL.sh
+++ install-arch-PERSONAL.sh
@@ -7,9 +7,14 @@ rm -rf yay-bin
 
 sudo ln -snf ~/repos/wayland-dotfiles/dotconfig/* ~/.config/
 
-yay -S linux-lts-headers nvidia-open-dkms hyprland-nvidia-git xdg-desktop-portal-hyprland libva-nvidia-driver-git easyeffects kitty hyprpicker ttf-ms-fonts ttf-apple-emoji ttf-google-sans ttf-jetbrains-mono-nerd neofetch polkit-gnome ffmpeg neovim dunst rofi pavucontrol starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer playerctl noise-suppression-for-voice waybar-hyprland-git wlogout swaylock-effects sddm-git nwg-look-bin layan-gtk-theme-git lxappearance qt5ct qt6ct pamixer brave-bin webcord-git-screenshare obs-studio --noconfirm
+yay -S linux-lts-headers nvidia-open-dkms hyprland-nvidia-git xdg-desktop-portal-hyprland-git libva-nvidia-driver-git easyeffects kitty hyprpicker ttf-ms-fonts ttf-apple-emoji ttf-google-sans ttf-jetbrains-mono-nerd neofetch polkit-gnome ffmpeg neovim dunst rofi pavucontrol starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer playerctl noise-suppression-for-voice waybar-hyprland-git wlogout swaylock-effects sddm-git nwg-look-bin layan-gtk-theme-git lxappearance qt5ct qt6ct pamixer brave-bin webcord-git-screenshare ntfs onedrivegui ark obs-studio
 
+sudo mkdir /mnt/4tb
+sudo mkdir /mnt/2tb
 sudo cp -f fstab /etc/ 
+sudo systemctl daemon-reload
+sudo mount -a
+
 sudo cp -f environment /etc/
 sudo cp -f mkinitcpio.conf /etc/
 mkdir ~/.config/autostart
@@ -18,6 +23,10 @@ sudo cp personal/easyeffects-service.desktop ~/.config/autostart
 sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
 
 sudo cp -f nvidia.conf /etc/modprobe.d/nvidia.conf
+
+sudo ln -snf /mnt/2tb/Downloads ~/
+sudo ln -snf /mnt/4tb/OneDrive/Documents ~/
+sudo ln -snf /mnt/4tb/OneDrive/Pictures ~/
 
 sudo systemctl enable sddm
 sudo systemctl start sddm
