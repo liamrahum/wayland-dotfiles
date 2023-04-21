fa69e8a8501f0303f39d8f99c403774654c58814
Liam Rahum <45101400+liamrahum@users.noreply.github.com>
2023-04-21 09:38:54 +0300

    Update install-arch-NVIDIA-LTS.sh
    Script now looks nicer, separated non-necessary

--- install-arch-NVIDIA-LTS.sh
+++ install-arch-NVIDIA-LTS.sh
@@ -15,27 +15,37 @@ cd wayland-dotfiles
 
 sudo ln -snf ~/repos/wayland-dotfiles/dotconfig/* ~/.config/
 
-echo "Necessary packages"
+clear
+echo "Installing necessary packages"
 sleep(2)
 yay -S linux-lts-headers nvidia-open-dkms hyprland-nvidia-git xdg-desktop-portal-hyprland-git libva-nvidia-driver-git waybar-hyprland-git
 
-echo "Wayland specifics"
+clear
+echo "Installing Wayland specifics"
 sleep(2)
 yay -S wl-clipboard wf-recorder hyprpicker swaybg wlogout swaylock-effects 
 
-echo "Some fonts to start with"
+clear
+echo "Installing some nice fonts"
 sleep(2)
 yay -S ttf-ms-fonts ttf-apple-emoji ttf-google-sans ttf-jetbrains-mono-nerd
 
-echo "General setup"
+clear
+echo "General applications to start with"
 sleep(2)
-yay -S dex easyeffects kitty neofetch polkit-gnome neovim dunst rofi pavucontrol grimblast-git playerctl sddm-git nwg-look-bin layan-gtk-theme-git lxappearance qt5ct qt6ct pamixer brave-bin webcord-git-screenshare ntfs3-dkms ark steam-native-runtime obs-studio
+yay -S dex kitty neofetch polkit-gnome neovim dunst rofi pavucontrol grimblast-git playerctl sddm-git nwg-look-bin layan-gtk-theme-git lxappearance qt5ct qt6ct pamixer
+
+clear
+read -p "Would you like to install Brave, Dolphin, WebCord, and ntfs? (y/N)?" BDWN
+if [ "$BDWN" = "y" || "" = "Y" ]; then
+yay -S dolphin brave-bin ntfs3-dkms webcord-git-screenshare;
+fi
 
 clear
 echo "WARNING! This will break your system if you're not the author of the git repo!"
 read -p "Would you like to skip the personal part? (Y/n)?" SKIP
 if [ "$SKIP" = "n" || "" = "N" ]; then
-  else
+  yay -S calf-git easyeffects
   sudo mkdir /mnt/4tb;
   sudo mkdir /mnt/2tb;
   sudo cp -f fstab /etc/; 
